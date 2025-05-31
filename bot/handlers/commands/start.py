08197de7ed3b141fd286c08a9944b7b5510bcdd2
Armemius <rusgy@yandex.ru>
2025-06-01 00:36:08 +0300

    feat: edit start sequence
    Edit message sequence for start command

--- bot/handlers/commands/start.py
+++ bot/handlers/commands/start.py
@@ -1,10 +1,12 @@
 import time
 
-from telegram import Update
+from telegram import Update, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from telegram.constants import ParseMode, ChatAction
 from telegram.ext import ContextTypes
 
 from decorators.command_handler import command_handler
+from utils.constants import PROFILE_KEYBOARD_BUTTON, SUBSCRIPTION_KEYBOARD_BUTTON, HELP_KEYBOARD_BUTTON, \
+    SUPPORT_KEYBOARD_BUTTON
 
 
 @command_handler(name="start")
@@ -12,32 +14,50 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Handler for /start command"""
     user_id = update.effective_user.id
 
-    await context.bot.send_message(
+    await update.message.reply_text(
         text="""
 👋 Привет\\! Я *Агромант* — бот, который поможет тебе в уходе за растениями и в садоводстве
         """,
-        chat_id=user_id,
         parse_mode=ParseMode.MARKDOWN_V2
     )
+
+    await update.message.delete()
 
     await context.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
     time.sleep(4)
 
-    await context.bot.send_message(
+    await update.message.reply_text(
         text="""
 Иногда цветы и растения могут болеть и это всегда грустно\\. Но я помогу тебе определить, что с ними не так, и как это исправить
         """,
-        chat_id=user_id,
         parse_mode=ParseMode.MARKDOWN_V2
     )
-
     await context.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
     time.sleep(4)
 
-    await context.bot.send_message(
+    layout = ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                PROFILE_KEYBOARD_BUTTON,
+                SUBSCRIPTION_KEYBOARD_BUTTON
+            ],
+            [
+                HELP_KEYBOARD_BUTTON
+            ],
+            [
+                SUPPORT_KEYBOARD_BUTTON
+            ]
+        ],
+        resize_keyboard=True,
+        is_persistent=False,
+        one_time_keyboard=False,
+        input_field_placeholder="Отправь фото в чат..."
+    )
+
+    await update.message.reply_text(
         text="""
-Для того чтобы подготовить рекомендации мне достаточно всего лишь парочки фотографий растения, которое тебя беспокоит
+Для того чтобы начать мне достаточно всего лишь парочки фотографий растения, которое тебя беспокоит
             """,
-        chat_id=user_id,
-        parse_mode=ParseMode.MARKDOWN_V2
+        parse_mode=ParseMode.MARKDOWN_V2,
+        reply_markup=layout
     )

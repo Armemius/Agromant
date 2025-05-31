7539c20b267cbad218bc3a90034ac44154a3bcec
Armemius <rusgy@yandex.ru>
2025-05-31 19:27:16 +0300

    feat: add utils & config parsing

---
+++ bot/utils/config.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+import os
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+
+
+def get_value(key: str, mandatory: bool = False) -> str:
+    value = os.getenv(key)
+    if value is None and mandatory:
+        raise ValueError(f"Environment variable {key} is not set")
+    return value
+
+def get_flag(key: str, default: bool = False) -> bool:
+    value = os.getenv(key)
+    if value is None:
+        return default
+    return value.lower() in ("1", "true", "yes")
+
+class Config:
+    def __init__(self):
+        self.bot_key = get_value("TG_BOT_API_KEY", mandatory=True)
+        self.proxyai_api = get_value("PROXY_AI_API_KEY", mandatory=True)
+        self.use_webhooks = get_flag("USE_BOT_WEBHOOKS", default=False)
+        self.webhook_url = get_value("WEBHOOK_URL", mandatory=self.use_webhooks)
+        self.webhook_path = get_value("WEBHOOK_PATH", mandatory=self.use_webhooks)
+        self.webhook_connections = int(get_value("WEBHOOK_CONNECTIONS", mandatory=self.use_webhooks) or 10)
+        self.webhook_port = int(get_value("WEBHOOK_PORT", mandatory=self.use_webhooks) or 8443)
+
+
+def init_config():
+    global bot_config
+
+bot_config = Config()

0f96b69a2a8cdf8b59206e28d335d4ef57b62403
Armemius <rusgy@yandex.ru>
2025-06-01 00:40:34 +0300

    feat: implement support command
    Add support command logic and wire into bot

---
+++ bot/handlers/commands/support.py
@@ -0,0 +1,19 @@
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.ext import ContextTypes
+
+from decorators.command_handler import command_handler
+from utils.constants import SUPPORT_KEYBOARD_BUTTON
+
+
+@command_handler(name="support", aliases=[SUPPORT_KEYBOARD_BUTTON])
+async def support(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.delete()
+
+    await update.message.reply_text(
+        """
+🛟 Если у тебя возникли вопросы или предложения по работе бота, ты можешь обратиться в [службу поддержки](https://t.me/armemius)
+        """,
+        parse_mode=ParseMode.MARKDOWN,
+        disable_web_page_preview=True,
+    )

63159e742a057ceb9220d02e3a08ed7542dc1131
Armemius <rusgy@yandex.ru>
2025-06-01 00:41:17 +0300

    feat: implement help command
    Add help command logic and wire into bot

---
+++ bot/handlers/commands/help.py
@@ -0,0 +1,32 @@
+from telegram import Update
+from telegram.constants import ChatAction
+from telegram.ext import ContextTypes
+
+from decorators.command_handler import command_handler
+from utils.constants import HELP_KEYBOARD_BUTTON
+
+
+with open("resources/demo.mp4", "rb") as file:
+    DEMO_VIDEO = file.read()
+
+
+@command_handler(name="help", aliases=[HELP_KEYBOARD_BUTTON])
+async def guide(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.delete()
+
+    await context.bot.send_chat_action(
+        update.effective_user.id,
+        ChatAction.UPLOAD_VIDEO,
+    )
+    await update.message.reply_video(
+        video=DEMO_VIDEO,
+        caption="""
+Пожалуйста, загрузите хорошую фотографию растения или его листьев
+
+• Чёткий фокус на повреждённых участках или характерных признаках (пятна, изменение цвета, деформация и т.д.)
+• Снимок при естественном освещении, без бликов и резких теней
+• При возможности — несколько ракурсов: общий вид растения и крупный план проблемных зон
+
+Как только фото будет загружено, я определю, что это за растение, опишу возможные проблемы и предложу варианты их решения
+        """
+    )

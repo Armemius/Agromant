fa3c08c9453fec50888e578e99562afa38f3c2f7
Armemius <rusgy@yandex.ru>
2025-06-01 22:26:20 +0300

    feat: add fields into config
    Update bot config with fields related to proper work with MongoDB

--- bot/tg/utils/config.py
+++ bot/tg/utils/config.py
@@ -11,11 +11,13 @@ def get_value(key: str, mandatory: bool = False) -> str:
         raise ValueError(f"Environment variable {key} is not set")
     return value
 
+
 def get_flag(key: str, default: bool = False) -> bool:
     value = os.getenv(key)
     if value is None:
         return default
     return value.lower() in ("1", "true", "yes")
+
 
 class Config:
     def __init__(self):
@@ -26,9 +28,15 @@ class Config:
         self.webhook_path = get_value("WEBHOOK_PATH", mandatory=self.use_webhooks)
         self.webhook_connections = int(get_value("WEBHOOK_CONNECTIONS", mandatory=self.use_webhooks) or 10)
         self.webhook_port = int(get_value("WEBHOOK_PORT", mandatory=self.use_webhooks) or 8443)
+        self.mongo_host = get_value("MONGO_HOST") or "localhost"
+        self.mongo_port = int(get_value("MONGO_PORT") or 27017)
+        self.mongo_username = get_value("MONGO_ROOT_USERNAME", mandatory=True)
+        self.mongo_password = get_value("MONGO_ROOT_PASSWORD", mandatory=True)
+        self.mongo_database = get_value("MONGO_DATABASE", mandatory=True)
 
 
 def init_config():
     global bot_config
+
 
 bot_config = Config()
